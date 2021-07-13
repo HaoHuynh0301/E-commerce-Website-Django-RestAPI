a687bb433919c2cae75cf88a9b15522fdf811178
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-07-13 17:38:25 +0700

    feat [react-js]: - add new navigation component - define a router

---
+++ front-end/src/components/Navigation.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+  } from "react-router-dom";
+// import Modal from "react-bootstrap/Modal";
+
+class Navigation extends React.Component {
+    render() {
+        return (
+            <div>
+                <nav id="side-nav">
+                    <i className="fas fa-bars"/>
+                    <div className="side-nav">
+                        <Link to="#">Home</Link>
+                        <Link to="#">Shop</Link>
+                        <Link to="#">Contact</Link>
+                        <Link to="#">News</Link>
+                    </div>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Navigation;
