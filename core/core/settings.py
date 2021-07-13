d057ce6b657e8b817d6613f23309b0699a84e770
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-07-13 17:20:46 +0700

    feat: create new front-end app by react-js.

--- core/core/settings.py
+++ core/core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api'
 ]
@@ -168,3 +169,7 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
